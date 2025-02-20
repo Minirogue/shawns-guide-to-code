47a1df15ce52f083513b8091231217c3771c7d04
shwitte <ettiwsl@gmail.com>
2025-02-19 22:14:20 -0800

    Add link to PR when PR number is available in the commit message

--- hooks/add-git-history.py
+++ hooks/add-git-history.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import sys
 from typing import Union
@@ -5,21 +6,32 @@ from typing import Union
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.structure.files import Files
 from mkdocs.structure.pages import Page
+from typing_extensions import Match
 
-exceptions = ["index", "version-history"]
+exceptions = []  # ["index", "version-history"]
+
 
 def on_page_markdown(
         markdown: str, page: Page, config: MkDocsConfig, files: Files
 ) -> Union[str, None]:
-    git_log_output = subprocess.check_output(["git", "log", "--oneline", "--", page.file.abs_src_path]).decode(sys.stdout.encoding)
     if page.file.name in exceptions:
         return markdown
     else:
+        git_hashes = (subprocess.check_output(["git", "log", "--format=%h", "--", page.file.abs_src_path])
+                      .decode(sys.stdout.encoding)
+                      .splitlines())
         commits_section = "\n\n## Page History \n\n"
-        for line in git_log_output.splitlines():
-            line_components = line.split()
-            commit_hash = line_components[0]
-            commit_message = line_components[1:]
-            commits_section += "- <a href=\"https://github.com/Minirogue/shawns-guide-to-code/commit/" + commit_hash +"\">" + commit_hash + "</a> " + " ".join(commit_message)+ "\n"
+        for commit_hash in git_hashes:
+            commit_link = "<a href=\"https://github.com/Minirogue/shawns-guide-to-code/commit/" + commit_hash + "\">" + commit_hash + "</a>"
+            commit_info = (subprocess.check_output(["git", "show", "--no-patch", "--format=\"%cs %h %s\"", commit_hash])
+                           .decode(sys.stdout.encoding))
+            commit_info = re.sub("\(#\\d+\)", lambda x: get_pr_link(x), commit_info) # add link to PR
+            commits_section += ("- " + commit_info.replace(commit_hash, commit_link)
+                                .replace("\"", ""))  # remove the vestigial quotation marks the formatting
         return markdown + commits_section
 
+
+def get_pr_link(match: Match[str]) -> str:
+    pr_number = match.group()[2:-1]
+    replacement_string = "(<a href=\"https://github.com/Minirogue/shawns-guide-to-code/pull/" + pr_number + "\">#" + pr_number + "</a>)"
+    return replacement_string
